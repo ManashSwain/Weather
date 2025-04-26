ea1215699a1dd0baebc9c61a9b93af93851c26aa
ManashSwain <manash0802@gmail.com>
2025-04-26 19:15:51 +0530

    implement api functionality metrics

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 
 const Weather = () => {
+
+    // search functionality 
   const search = async (city) => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
         import.meta.env.VITE_WEATHER_API
       }`;
       const response = await fetch(url);
